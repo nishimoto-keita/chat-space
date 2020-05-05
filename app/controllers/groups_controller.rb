aa15bff3613d6b5306d31e61f0561b14d1fa8821
nishimoto-keita <aggrest0306@gmail.com>
2020-05-05 16:46:58 +0900

    Revert "グループ名、ユーザー名表示"
    This reverts commit ce75f13387d83b1b4e5ef02c3d4d3f50b4def1c2.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,11 +30,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  def show
-    @group = Group.find(params[:id])
-  end
-
-
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
